f702a9772d07108c8b4bf7c463b741ff43af17aa
Juan Pablo Garzon Parra <Juan-Pablo.Garzon@endava.com>
2024-02-15 12:16:13 -0500

    Impossible done

--- 4. Impossible - Longest Valid Parentheses/LongestValidParentheses.py
+++ 4. Impossible - Longest Valid Parentheses/LongestValidParentheses.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-
+        stack = [-1]
+        longest =0
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            elif s[i] == ")":
+                stack.pop()
+            if len(stack) == 0:
+                stack.append(i)
+            elif len(stack) != 0:
+                longest = max(longest, i-stack[-1])
+        return longest
